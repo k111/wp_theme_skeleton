6ee2b2477a9335af7af88c3d7a02632a5848d719
Ishida Keiichi <okeke1101@gmail.com>
2015-10-26 16:47:59 +0900

    add template

---
+++ 404.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+
+<article>
+<h1 class="text-center">Not Found</h1>
+<p class="text-center">お探しのページは見つかりませんでした。</p>
+</article>
+
+<?php get_footer(); ?>
+
---
+++ page.php
@@ -0,0 +1,25 @@
+<?php // 固定ページ テンプレート
+get_header(); ?>
+
+<div class="row">
+<div class="col-md-9">
+
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+<h1 class="page-header"><?php the_title(); ?></h1>
+
+<?php the_content(); ?>
+
+<pre>
+<?php print_r( $post ); ?>
+</pre>
+
+<?php endwhile; endif;?>
+
+<!-- /.col-md-9 --></div>
+
+
+<?php get_sidebar(); ?>
+
+<!-- /.row --></div>
+
+<?php get_footer(); ?>
