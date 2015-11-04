77b190b4b3b4496bcb567b0eea5ce610820f07c1
Ishida Keiichi <okeke1101@gmail.com>
2015-11-04 21:05:11 +0900

    edit comment

--- front-page.php
+++ front-page.php
@@ -1,6 +1,7 @@
 <?php get_header(); ?>
 
-<?php get_template_part('carousel','top'); ?>
+<?php // カスタム投稿バナーパーツ読み込み
+// get_template_part('carousel','top'); ?>
 
 <div class="jumbotron">
 <h1><?php bloginfo('name'); ?></h1>
@@ -8,9 +9,8 @@
 </div>
 
 
-<section>
-<h2>お知らせ</h2>
-<dl class="dl-horizontal">
+<div class="container">
+<h2 class="text-center page-header">Infomation</h2>
 <?php // 投稿ページ最新5件を表示
 $args = array(
     'post_type' => 'post',
@@ -19,13 +19,34 @@ $args = array(
 $post_list = get_posts( $args );
 foreach ( $post_list as $post ) : setup_postdata( $post );
 ?>
-    <dt><?php the_time('Y年m月d日'); ?></dt>
+<dl class="dl-horizontal">
+    <dt><time datetime="<?php the_time('Y-m-d'); ?>"><?php the_time('Y年m月d日'); ?></time></dt>
     <dd><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></dd>
+</dl>
 <?php endforeach;
 wp_reset_postdata();
 ?>
-</dl>
-</section>
+<!-- /.container --></div>
 
+<div class="row" style="margin-bottom:5em;">
+<div class="col-md-4">
+<h2 class="text-center">Works</h2>
+<p class="text-center h1"><i class="glyphicon glyphicon-education"></i></p>
+<p>説明が入ります。説明が入ります。説明が入ります。説明が入ります。説明が入ります。説明が入ります。説明が入ります。説明が入ります。</p>
+<p class="text-center"><a href="#" class="btn btn-primary" role="button">詳細をみる</a></p>
+<!-- /.col-md-4 --></div>
+<div class="col-md-4">
+<h2 class="text-center">Service</h2>
+<p class="text-center h1"><i class="glyphicon glyphicon-cloud"></i></p>
+<p>説明が入ります。説明が入ります。説明が入ります。説明が入ります。説明が入ります。説明が入ります。説明が入ります。説明が入ります。</p>
+<p class="text-center"><a href="#" class="btn btn-primary" role="button">詳細をみる</a></p>
+<!-- /.col-md-4 --></div>
+<div class="col-md-4">
+<h2 class="text-center">About</h2>
+<p class="text-center h1"><i class="glyphicon glyphicon-grain"></i></p>
+<p>説明が入ります。説明が入ります。説明が入ります。説明が入ります。説明が入ります。説明が入ります。説明が入ります。説明が入ります。</p>
+<p class="text-center"><a href="#" class="btn btn-primary" role="button">詳細をみる</a></p>
+<!-- /.col-md-4 --></div>
+<!-- /.row --></div>
 
 <?php get_footer(); ?>
